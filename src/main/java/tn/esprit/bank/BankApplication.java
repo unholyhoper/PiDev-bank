9e1c6cb8aba8acc75311560a9e2d30dc06bdc1ad
hajedkh <kh.hajed@gmail.com>
2022-07-23 09:51:14 +0100

    Revert: Account services and mailing (74287801)

--- src/main/java/tn/esprit/bank/BankApplication.java
+++ src/main/java/tn/esprit/bank/BankApplication.java
@@ -2,21 +2,12 @@ package tn.esprit.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import tn.esprit.bank.repository.AccountRequestRepository;
 
 @SpringBootApplication
 public class BankApplication {
 
 	public static void main(String[] args) {
-	ConfigurableApplicationContext context =  SpringApplication.run(BankApplication.class, args);
-
-		AccountRequestRepository accountRequestRepository = context.getBean(AccountRequestRepository.class);
-
-		System.out.println(accountRequestRepository.findAll());
-
-
-
+		SpringApplication.run(BankApplication.class, args);
 	}
 
 }
